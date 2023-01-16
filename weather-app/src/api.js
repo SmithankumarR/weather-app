4c3fa216bffc5bbb9e8729d7827ad1aed34ea535
SmithanKumarR <smithankumar404@gmail.com>
2023-01-16 14:22:18 +0530

    weather app -- modify -forecast

---
+++ weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "946604312emshdbe6b3c70f7a608p1fceb5jsn95c6dc73fe94",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "b5eaa2cc2bc5620fe382ecc4068ae504"; // enter your key from openweather API
+  
